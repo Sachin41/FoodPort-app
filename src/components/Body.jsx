b8c55aa14d3b11cf53f32d98a12b480ad500b2cd
SKumar <sk963451@gmail.com>
2026-04-30 12:45:18 +0530

    case changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import restaurantLists from '../config/mockData';
 import Shimmer from './shimmer';
-import Restaurantcard from './Restaurantcard';
+import Restaurantcard from './RestaurantCard';
 import {
   SWIGGY_API_URL,
   SWIGGY_REST_API_PATH,
